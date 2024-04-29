58c8e1db125d10bd9737dfdcd26768056b0ea478
PallAviDevelop <pallavi.prodev@gmail.com>
2024-04-29 11:45:05 +0530

    certi logo added

--- src/app/data/certificate.js
+++ src/app/data/certificate.js
@@ -16,7 +16,7 @@ export const certificate = [
     text: "It is a long established fact that a reader will be distracted by the readable.",
   },
   {
-    imgcert: "/images/Group 65.png",
+    imgcert: "/images/image 12.png",
     text: "It is a long established fact that a reader will be distracted by the readable.",
   },
   {
