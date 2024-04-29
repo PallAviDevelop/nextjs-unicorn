3ffa50f722a579611294e46907380e90ce467dd7
PallAviDevelop <pallavi.prodev@gmail.com>
2024-04-29 11:35:43 +0530

    commit more changes for broken images

--- src/app/data/certificate.js
+++ src/app/data/certificate.js
@@ -1,84 +1,84 @@
 export const certificate = [
   {
-    imgcert: "/../images/hal.png",
+    imgcert: "/images/hal.png",
     text: "It is a long established fact that a reader will be distracted by the readable.",
   },
   {
-    imgcert: "/../images/IFFS-3 1.png",
+    imgcert: "/images/IFFS-3 1.png",
     text: "It is a long established fact that a reader will be distracted by the readable.",
   },
   {
-    imgcert: "/../images/image 11.png",
+    imgcert: "/images/image 11.png",
     text: "It is a long established fact that a reader will be distracted by the readable.",
   },
   {
-    imgcert: "/../images/image 13 (1).png",
+    imgcert: "/images/image 13 (1).png",
     text: "It is a long established fact that a reader will be distracted by the readable.",
   },
   {
-    imgcert: "/../images/Group 65.png",
+    imgcert: "/images/Group 65.png",
     text: "It is a long established fact that a reader will be distracted by the readable.",
   },
   {
-    imgcert: "/../images/hal.png",
+    imgcert: "/images/hal.png",
     text: "It is a long established fact that a reader will be distracted by the readable.",
   },
   {
-    imgcert: "/../images/image 13.png",
+    imgcert: "/images/image 13.png",
     text: "It is a long established fact that a reader will be distracted by the readable.",
   },
   {
-    imgcert: "/../images/image 14.png",
+    imgcert: "/images/image 14.png",
     text: "It is a long established fact that a reader will be distracted by the readable.",
   },
   {
-    imgcert: "/../images/image 15.png",
+    imgcert: "/images/image 15.png",
     text: "It is a long established fact that a reader will be distracted by the readable.",
   },
   {
-    imgcert: "/../images/image 16.png",
+    imgcert: "/images/image 16.png",
     text: "It is a long established fact that a reader will be distracted by the readable.",
   },
   {
-    imgcert: "/../images/image17.png",
+    imgcert: "/images/image17.png",
     text: "It is a long established fact that a reader will be distracted by the readable.",
   },
   {
-    imgcert: "/../images/image 18.png",
+    imgcert: "/images/image 18.png",
     text: "It is a long established fact that a reader will be distracted by the readable.",
   },
 ];
 export const bestseller = [
   {
-    imgseller: "/../images/red.png",
+    imgseller: "/images/red.png",
     name:'Red Snapper',
   },
   {
-    imgseller: "/../images/Mask group.png",
+    imgseller: "/images/Mask group.png",
     name:'Salmon',
   },
   {
-    imgseller: "/../images/Mask group (2).png",
+    imgseller: "/images/Mask group (2).png",
     name:'Rohu',
   },
   {
-    imgseller: "/../images/tun.png",
+    imgseller: "/images/tun.png",
     name:'Tuna',
  },
   {
-    imgseller: "/../images/black.png",
+    imgseller: "/images/black.png",
     name:'Black Pomfret',
   },
   {
-    imgseller: "/../images/robin.png",
+    imgseller: "/images/robin.png",
     name:'Ribbon Fish',
   },
   {
-    imgseller: "/../images/sea.png",
+    imgseller: "/images/sea.png",
     name:'Seabass',
   },
   {
-    imgseller: "/../images/shri.png",
+    imgseller: "/images/shri.png",
     name:'Freshwater Shrimps',
   },
 ];
