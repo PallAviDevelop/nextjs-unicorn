d5191dee870e2f45d5fd171f9ec9f49da0f77630
PallAviDevelop <pallavi.prodev@gmail.com>
2024-05-01 14:48:31 +0530

    added

--- src/app/components/Herosection.js
+++ src/app/components/Herosection.js
@@ -132,34 +132,7 @@ const Herosection = ({ title, heading, des }) => {
           </div>
         </div>
       </section>
-      <section>
-      <figure class="md:flex bg-slate-100 rounded-xl p-8 md:p-0 dark:bg-slate-800 justify-center">
-  {/* <img class="w-24 h-24 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto" src="/sarah-dayan.jpg" alt="" width="384" height="512"/> */}
-  <Image
-                  className="w-24 h-24 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto"
-                  src="/Fish1.png"
-                  width="384" height="512"
-                  alt="logo"
-                />
-  <div class="pt-6 md:p-8 text-center md:text-left space-y-4">
-    <blockquote>
-      <p class="text-lg font-medium">
-        “Tailwind CSS is the only framework that I've seen scale
-        on large teams. It’s easy to customize, adapts to any design,
-        and the build size is tiny.”
-      </p>
-    </blockquote>
-    <figcaption class="font-medium">
-      <div class="text-sky-500 dark:text-sky-400">
-        Sarah Dayan
-      </div>
-      <div class="text-slate-700 dark:text-slate-500">
-        Staff Engineer, Algolia
-      </div>
-    </figcaption>
-  </div>
-</figure>
-      </section>
+      
       <section className="container">
         <div className="w-[90%] md:w-[80%] mx-auto  grid grid-cols-1 md:grid-cols-1 lg:grid-cols-1">
           <h2 className="mt-[1rem] text-center text-[28px] text-black font-semibold mb-4">
